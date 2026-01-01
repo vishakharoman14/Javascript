7db19ac571460cb9686bb807d690e83bfe63ee2b
Vishakha Roman <Vishakha.Roman@ibm.com>
2026-01-01 23:41:48 +0530

    var, let & const intro

---
+++ Basicss/script.js
@@ -0,0 +1,28 @@
+// var, let, const - line by line comaprision
+// var b;
+// a=12; //its a bug in javascript which is not appriciated
+// var a = 12;
+// let a=12;
+// let a;
+// const a=12;
+// const a; 
+
+//Declarations & Initialization
+
+// var a;  // decare karna 
+// var a=12 // declare and initialize
+// +++limitations/issues with var+++
+// windows mein add hota hai .
+//function scoped hota hai 
+//firse declare kar sakte hao same name se and error nahi ayega
+
+// var a=14;
+//let is more secure  hai 
+// let a;
+// let a=12;
+// let a=15;
+
+//const isliye ki hame pata hai pure code main const ki valu enahi badlegi
+// const a=12;
+
+
